2bdd258010753a1fd7b7c945ffaa671517e9fb25
siduck <siduck@tutanota.com>
2023-10-14 07:40:20 +0530

    add brand logo & theme toggle ui

--- src/components/sidebar.svelte
+++ src/components/sidebar.svelte
@@ -1,7 +1,9 @@
 <script>
+  import Btn from "@/components/ui/button.svelte";
+  import ThemeToggle from "@/components/ui/themeToggle.svelte";
   import { activeSection } from "@/store";
 
-  let btns = [
+  let sections = [
     {
       name: "Dashboard",
       icon: "i-fluent:grid-circles-24-filled",
@@ -24,16 +26,40 @@
   ];
 </script>
 
-<aside p="5 l2 r15" shadow-xl>
-  {#each btns as btn}
-    <button
-      class="btnglass"
-      gap3
-      text={$activeSection == btn.name ? "slate7" : "!slate5"}
-      on:click={() => ($activeSection = btn.name)}
-    >
-      <div class={btn.icon}></div>
-      {btn.name}
-    </button>
+<aside p="2 l2 " shadow-xl class="flex flex-col">
+  <Btn
+    icon="i-icon-park-solid:flashlamp"
+    class="btnglass text-2xl !w-full justify-start rounded-none"
+    text="TaskCrafty"
+    font="bold"
+    border="0 b solid slate2"
+    mb={3}
+    p="t3 b5"
+    onClick={() => ($activeSection = "Dashboard")}
+  />
+
+    <!-- section btns -->
+  {#each sections as btn}
+    <Btn
+      class={`btnglass w-full justify-start  ${
+        $activeSection == btn.name ? "text-slate8" : "!text-slate5"
+      }`}
+      text={btn.name}
+      icon={btn.icon}
+      onClick={() => ($activeSection = btn.name)}
+      hover="bg-slate-50"
+      p="r16"
+    />
   {/each}
+
+    <!-- settings btn & theme toggle -->
+  <flex mt="auto" gap2>
+    <Btn
+      icon="i-mingcute:settings-1-line"
+      class="!w-full"
+      text="Settings"
+      hover="bg-slate-50"
+    />
+    <ThemeToggle />
+  </flex>
 </aside>
